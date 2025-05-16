f6b01a0ab3e8ec95ee21c0bcf7feea470d13e40b
gbrh12 <erwin@promoted.is>
2025-05-16 14:26:44 +0000

    Added alphabet to WordGame class

--- src/main/java/com/example/WordGame.java
+++ src/main/java/com/example/WordGame.java
@@ -5,6 +5,7 @@ import java.util.Map;
 public class WordGame {
     private Map<Character, Integer> POINTS = Utils.getScoreMap();
     private Map<Character, Integer> DIST = Utils.getDistributionMap();
+    private char[] ALPHABET = Utils.getEnglishAlphabet();
 
     public int scoreWord(String word) {
        return 0;
